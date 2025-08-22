7515efb81905550602618c67aa48055a61c01f14
zhangqingshan <1368816027@qq.com>
2025-08-22 18:14:29 +0800

    第五章-应用模块（优化） 1.增加预览、部署能力

--- src/main/java/com/yupi/yuaicodemother/service/AppService.java
+++ src/main/java/com/yupi/yuaicodemother/service/AppService.java
@@ -4,7 +4,9 @@ import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.yupi.yuaicodemother.model.dto.app.AppQueryRequest;
 import com.yupi.yuaicodemother.model.entity.App;
+import com.yupi.yuaicodemother.model.entity.User;
 import com.yupi.yuaicodemother.model.vo.AppVO;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -20,7 +22,7 @@ public interface AppService extends IService<App> {
      * @param app
      * @return
      */
-    public AppVO getAppVO(App app);
+    AppVO getAppVO(App app);
 
 
     /**
@@ -28,13 +30,30 @@ public interface AppService extends IService<App> {
      * @param appList
      * @return
      */
-    public List<AppVO> getAppVOList(List<App> appList);
+    List<AppVO> getAppVOList(List<App> appList);
 
     /**
      * 构造应用查询条件
      * @param appQueryRequest
      * @return
      */
-    public QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
+    /**
+     * 通过对话生成应用代码
+     * @param appId 应用id
+     * @param message 提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+
+    /**
+     * 应用部署
+     * @param appId 应用id
+     * @param loginUser 登录用户
+     * @return
+     */
+    String deployApp(Long appId, User loginUser);
 }
